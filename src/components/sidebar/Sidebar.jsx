9f1f3190c6f4e62256cf7733bb5a5da8576ace57
BrianSimiyu <brianmaruti@gmail.com>
2022-04-02 15:13:11 +0300

    react icons added

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { FiUsers } from "react-icons/fi";
+import { AiTwotoneShop } from "react-icons/ai";
+import { FaTruckMoving } from "react-icons/fa";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 import "./sidebar.scss";
 
 const Sidebar = () => {
@@ -11,16 +18,37 @@ const Sidebar = () => {
       <div className="center">
         <ul>
           <li>
+            <MdOutlineDashboard />
             <span>Dashboard</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <FiUsers />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <AiTwotoneShop />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <span>Orders</span>
+          </li>
+          <li>
+            <FaTruckMoving />
+            <span>Delivery</span>
+          </li>
+
+          <li>
+            <IoMdNotificationsOutline />
+            <span>Notifcations</span>
+          </li>
+
+          <li>
+            <CgProfile />
+            <span>Profile</span>
+          </li>
+          <li>
+            <RiLogoutBoxRLine />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
