ec3d0b345e5aeedc1935a50409974bf53b8c574b
BrianSimiyu <brianmaruti@gmail.com>
2022-04-03 23:47:11 +0300

    icon fix

--- src/components/widgets/Widget.jsx
+++ src/components/widgets/Widget.jsx
@@ -20,7 +20,15 @@ const Widget = ({ type }) => {
         title: "USERS",
         isMoney: false,
         link: "See all users",
-        icon: <FiUsers className="icon" />,
+        icon: (
+          <FiUsers
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
       };
       break;
     case "order":
@@ -28,7 +36,15 @@ const Widget = ({ type }) => {
         title: "ORDERS",
         isMoney: false,
         link: "View all orders",
-        icon: <FiShoppingCart className="icon" />,
+        icon: (
+          <FiShoppingCart
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218,165,32,0.2)",
+            }}
+          />
+        ),
       };
       break;
     case "earning":
@@ -36,7 +52,12 @@ const Widget = ({ type }) => {
         title: "EARNINGS",
         isMoney: true,
         link: "View net earnings",
-        icon: <MdOutlineAttachMoney className="icon" />,
+        icon: (
+          <MdOutlineAttachMoney
+            className="icon"
+            style={{ color: "green", backgroundColor: "rgba(0,128,0,0.2)" }}
+          />
+        ),
       };
       break;
     case "balance":
@@ -44,7 +65,12 @@ const Widget = ({ type }) => {
         title: "BALANCE",
         isMoney: true,
         link: "See details",
-        icon: <MdOutlineAccountBalanceWallet className="icon" />,
+        icon: (
+          <MdOutlineAccountBalanceWallet
+            className="icon"
+            style={{ color: "purple", backgroundColor: "rgba(128,0,128,0.2)" }}
+          />
+        ),
       };
       break;
     default:
