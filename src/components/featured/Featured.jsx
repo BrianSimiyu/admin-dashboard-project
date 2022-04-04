930d3bca00f896239f461cdd2de8f8d66d72fb51
BrianSimiyu <brianmaruti@gmail.com>
2022-04-04 18:24:43 +0300

    chart 1

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { HiDotsVertical } from "react-icons/hi";
 import "./featured.scss";
 
 const Featured = () => {
-  return <div className="featured">Featured</div>;
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <HiDotsVertical fontSize="small" />
+      </div>
+
+      <div className="bottom">
+        <div className="featuredChart"></div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">450 €</p>
+        <p className="description">
+          Previous transactions processing. Last payments may not be included
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
