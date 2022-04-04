409b64aa8337f3b593b7c53f5a838b0e7ad4834e
BrianSimiyu <brianmaruti@gmail.com>
2022-04-04 18:45:09 +0300

    toral revenue chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { HiDotsVertical } from "react-icons/hi";
+import { RiArrowUpSLine } from "react-icons/ri";
+
 import "./featured.scss";
 
 const Featured = () => {
@@ -17,6 +19,32 @@ const Featured = () => {
         <p className="description">
           Previous transactions processing. Last payments may not be included
         </p>
+
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <RiArrowUpSLine fontSize="small" />
+              <div className="resultAmount">€ 12.4k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last week</div>
+            <div className="itemResult positive">
+              <RiArrowUpSLine fontSize="small" />
+              <div className="resultAmount">€ 12.4k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <RiArrowUpSLine fontSize="small" />
+              <div className="resultAmount">€ 12.4k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
