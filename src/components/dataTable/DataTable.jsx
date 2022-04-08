0084741c79eda16349c4f4dac8e538f62b86eec7
BrianSimiyu <brianmaruti@gmail.com>
2022-04-08 16:46:27 +0300

    action added

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -4,11 +4,27 @@ import { userColumns, userRows } from "../../dataTableSource";
 import React from "./dataTable.scss";
 
 const DataTable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="dataTable">
       <DataGrid
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
