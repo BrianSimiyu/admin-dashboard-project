e3b8b1328dff38d33ca348ea2bdc240cf7abe9e2
BrianSimiyu <brianmaruti@gmail.com>
2022-04-25 21:37:50 +0300

    Form

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 import "./new.scss";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div className="new">
+      <Sidebar />
+
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="john_doe" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default New;
